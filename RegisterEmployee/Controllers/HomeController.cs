2885b9abff9e66bb55335257745b0a848e3f9a70
Neeraj19 <neerajchand19@gmail.com>
2021-01-14 13:34:39 +0530

    Update HomeController.cs

--- RegisterEmployee/Controllers/HomeController.cs
+++ RegisterEmployee/Controllers/HomeController.cs
@@ -1,15 +1,21 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using RegisterEmployee.DataAccessLayer;
-using RegisterEmployee.Models;
+using Newtonsoft.Json;
+using RegisterEmployee.Helper;
+using RegisterEmployeeAPI.Models;
 
 namespace RegisterEmployee.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        EmployeeAPI _api = new EmployeeAPI();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -23,20 +29,25 @@ namespace RegisterEmployee.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(Employee employee)
+        public async Task<ActionResult> Index(Employee employee)
         {
             try
             {
                 if (ModelState.IsValid)
                 {
-                    EmployeeDAL dal = new EmployeeDAL();
-                    string result = dal.CreateEmployee(employee);
-                    this.TempData["Result"] = result;
+                    HttpClient client = _api.Initial();
+                    HttpContent data = new StringContent(JsonConvert.SerializeObject(employee), Encoding.UTF8, "application/json");
+                    HttpResponseMessage result = await client.PostAsync("api/Employee/AddEmployee", data);
+                    if (result.IsSuccessStatusCode)
+                    {
+                        this.TempData["Result"] = "Register successfully.";
+                    }
+                    
                     ModelState.Clear();
                 }
             }
 
-            catch
+            catch(Exception ex)
             {
                 ModelState.AddModelError("", "Error in inserting data");
             }
@@ -52,7 +63,7 @@ namespace RegisterEmployee.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new RegisterEmployee.Models.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
